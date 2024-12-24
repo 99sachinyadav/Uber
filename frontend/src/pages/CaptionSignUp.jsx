be6db02a39260e64d5dccd2d7f7f5a7a8cda4129
99sachinyadav <sy7841846@gmail.com>
2024-12-24 07:39:40 +0530

    ongoing commit

--- frontend/src/pages/CaptionSignUp.jsx
+++ frontend/src/pages/CaptionSignUp.jsx
@@ -1,7 +1,7 @@
 import { useContext, useState } from "react";
 import { Link, useNavigate } from "react-router-dom"
 import axios from 'axios'
-import { CaptionDataContext } from "../context/captionContext";
+import { CaptionDataContext } from "../context/CaptionContext";
  
 
 import { backendUrl } from "../App";
