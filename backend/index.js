28d90f8b4ec946dfc16c3e4b2c666c7c3f816b1e
99sachinyadav <sy7841846@gmail.com>
2024-12-24 06:29:59 +0530

    render

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ import http from 'http'
  const app= express();
  const server = http.createServer(app);
  initializeSocket(server);
- const PORT=process.env.PORT
+ const PORT=process.env.PORT||4000
  app.use(cors())
  app.use(express.json());
  app.use(express.urlencoded({extended:true}))
